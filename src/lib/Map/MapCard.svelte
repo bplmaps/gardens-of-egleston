38ca4bbc16227254a23c9b29d1db9277f5b704a1
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2025-03-25 15:12:00 -0400

    ui tweaks

--- src/lib/Map/MapCard.svelte
+++ src/lib/Map/MapCard.svelte
@@ -3,10 +3,10 @@
 </script>
 
 
-<div class="bg-gray-100 rounded-lg border-4 border-gray-200 shadow-lg p-4 h-full">
+<div class="bg-gray-100 rounded-lg border-4 border-gray-200 shadow-lg p-4 h-full max-h-[30vh] overflow-x-scroll">
     <h1 class="text-2xl font-bold">{trail.name}</h1>
     <p>{trail.description}</p>
-    <div class="flex mt-4 gap-2">
+    <div class="flex flex-col md:flex-row mt-4 gap-2">
         <a href={trail.url} target="_blank" class="bg-blue-500 text-white px-2 py-1 rounded-md font-semibold flex items-center gap-1">
             Learn More
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
